41d02db6cf250fccd8b4ec68bd9ded5b316fb7b3
Hanz-kun <71994028+Hanz-kun@users.noreply.github.com>
2020-11-20 10:28:53 +0800

    Progress Nov. 20
    Home and room page done

--- hotel_bookingproj/pages/views.py
+++ hotel_bookingproj/pages/views.py
@@ -4,18 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-# for testing code  no html 
+# for testing code  no html
 #def home_view(request, *args, **kwargs):
  #   return HttpResponse("<h1> home page </h1> ")
 
 
 
-# html of home.html 
+# html of home.html
 
 def home_view(request, *args, **kwargs):
    print(args, kwargs)
    print(request.user)
    return render(request, 'home.html', {})
 
-
-
+def roompage_view(request, *args, **kwargs):
+   print(args, kwargs)
+   print(request.user)
+   return render(request, 'room page2.html', {})
