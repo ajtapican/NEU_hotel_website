52132d205552afb6fb4a348703d214cd751a5527
Hanz-kun <71994028+Hanz-kun@users.noreply.github.com>
2020-11-22 09:13:19 +0800

    Registration Update
    Nov 22, 2020

--- hotel_bookingproj/hotel_bookingproj/urls.py
+++ hotel_bookingproj/hotel_bookingproj/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages.views import home_view
 from pages.views import roompage_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', home_view, name ='home'),
+    path('users/', include('users.urls')),
     path('roompage', roompage_view, name='roompage'),
 ]
