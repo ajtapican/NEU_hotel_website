d74e33ca2d98aa5afc3da6f80c8b5c05abf7da29
Hanz-kun <71994028+Hanz-kun@users.noreply.github.com>
2020-12-03 10:43:05 +0800

    Working Login Form
    Dec 3, 2020

--- hotel_bookingproj/pages/forms.py
+++ hotel_bookingproj/pages/forms.py
@@ -5,4 +5,4 @@ class Info(forms.ModelForm):
     
     class Meta:
         model = Information
-        fields = ('name', 'email', 'address', 'city', 'phone', 'state', 'zip', 'comments', 'name1', 'cnumber', 'expm', 'expy', 'cvv', 'arrive', 'depart', 'guest', 'rooms')
+        fields = ('name', 'email', 'address', 'city', 'phone', 'zip')
--- hotel_bookingproj/pages/views.py
+++ hotel_bookingproj/pages/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from pages.models import Information
 from pages.forms import Info
